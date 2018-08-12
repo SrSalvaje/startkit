ca0af5748e68e8222b7f5adae1db9b9c20bfbf30
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2018-08-12 14:27:43 +0200

    setting gulp4

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,54 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
+//var autoprefixer = require('gulp-autoprefixer');
+//var browserSync = require('browser-sync').create();
+
+//SASS
+gulp.task('sass', function(){
+    return gulp.src('app/scss/.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+   /*  .pipe(browserSync.reload({
+        stream: true
+    })) */
+});
+
+/* gulp.task('browserSync', function(){
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        },
+    })
+})
+ */
+
+
+//gulp.task('watch', function(){
+       // gulp.watch('app/scss/**/*.scss', gulp.series('sass'));
+    //other watchers
+//});
+
+
+//watchers 
+//gulp.task('watch', ['browserSync', 'sass'], function(){
+
+    //gulp.watch('app/scss/**/*.scss', gulp.series('sass'));
+    //other watchers
+
+//})
+
+
+
+
+
+
+
+
+
+
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+/* gulp.task('serve', ['sass'], function() {
 
     browserSync.init({
         server: "./app"
@@ -14,31 +56,39 @@ gulp.task('serve', ['sass'], function() {
 
     gulp.watch("app/scss/*.scss", ['sass']);
     gulp.watch("app/*.html").on('change', browserSync.reload);
-});
+}); */
 
 // Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-    return gulp.src("app/scss/*.scss")
-        .pipe(sass())
-        .pipe(gulp.dest("app/css"))
-        .pipe(browserSync.stream());
-});
+ //gulp.task('styles', function() {
+   // return gulp.src('sass/**/*.scss')
+       // .pipe(sass().on('error', sass.logError))
+      //  .pipe(autoprefixer({
+         //   browsers: ['last 2 versions'],
+        //    cascade:false
+      //  }))
 
-gulp.task('default', ['serve']);
+     //   .pipe(gulp.dest('./css'))
+     //   .pipe(browserSync.stream());
+//});
+
+//gulp.task('default', ['serve']); 
 
 
-gulp.task('default', function() {
-	gulp.watch('sass/**/*.scss',['styles']); 
-});
 
-gulp.task('styles', gulp.series(function(done) {
+/////////////////////////////
 
-gulp.src('sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions']
-        }))
-        .pipe(gulp.dest('./css'));
+//gulp.task('default', function() {
+	//gulp.watch('sass/**/*.scss',['styles']); 
+//});
 
-done(); }));
+////////
+
+//gulp.task('styles', gulp.series(function(done) {
+
+//gulp.src('sass/**/*.scss')
+     
+       
+       // .pipe(gulp.dest('./css'));
+
+//done(); })); 
 
